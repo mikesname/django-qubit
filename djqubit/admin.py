e259ca13cf61135bba70626b4fa9375f8bddea3c
Mike Bryant <mikesname@gmail.com>
2011-09-03 17:18:56 +0100

    Exposed more models to the Admin site

--- djqubit/admin.py
+++ djqubit/admin.py
@@ -2,7 +2,11 @@
 Admin config for Qubit models.  This is not going to be comprehensive.
 """
 from django.contrib import admin
-from models import InformationObject, Event
+from models import InformationObject, Event, Actor, Repository, Term, Taxonomy
 
 admin.site.register(InformationObject)
 admin.site.register(Event)
+admin.site.register(Actor)
+admin.site.register(Repository)
+admin.site.register(Term)
+admin.site.register(Taxonomy)
