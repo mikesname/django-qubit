374bc1ec8d70803decf57cbf0276ba507e6d29af
Mike Bryant <mikesname@gmail.com>
2011-09-04 09:58:59 +0100

    Use admin as the root page for now

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ehridata.views.home', name='home'),
+    url(r'^', include(admin.site.urls)),
     # url(r'^ehridata/', include('ehridata.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
