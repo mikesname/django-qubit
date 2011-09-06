5f6efdc73c6a5dbe93254b717d34b0fc2dab90cf
Mike Bryant <mikesname@gmail.com>
2011-09-06 11:17:44 +0100

    Added more models to be introspected via the admin site

--- djqubit/admin.py
+++ djqubit/admin.py
@@ -2,7 +2,8 @@
 Admin config for Qubit models.  This is not going to be comprehensive.
 """
 from django.contrib import admin
-from models import InformationObject, Event, Actor, Repository, Term, Taxonomy
+from models import InformationObject, Event, Actor, Repository, Term, Taxonomy,\
+        User, Note, DigitalObject, Relation        
 
 admin.site.register(InformationObject)
 admin.site.register(Event)
@@ -10,3 +11,7 @@ admin.site.register(Actor)
 admin.site.register(Repository)
 admin.site.register(Term)
 admin.site.register(Taxonomy)
+admin.site.register(User)
+admin.site.register(Note)
+admin.site.register(DigitalObject)
+admin.site.register(Relation)
